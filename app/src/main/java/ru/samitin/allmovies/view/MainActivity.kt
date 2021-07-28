c826bdd90c90f9f21a5229305bf2b3766fa414db
samitin <samitin.e@gmail.com>
2021-07-28 16:13:39 +1100

    Начало работы

---
+++ app/src/main/java/ru/samitin/allmovies/view/MainActivity.kt
@@ -0,0 +1,18 @@
+package ru.samitin.allmovies.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.samitin.allmovies.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/ru/samitin/allmovies/viewmodel/MainViewModel.kt
@@ -0,0 +1,7 @@
+package ru.samitin.allmovies.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
