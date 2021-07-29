502a7e58d27bc4c3f8106dacc3be3ace96d40831
samitin <samitin.e@gmail.com>
2021-07-29 20:10:39 +1100

    список фильмов в fragment_home.xml пока с одним reciclerView

--- app/src/main/java/ru/samitin/allmovies/model/repository/RepositoryImpl.kt
+++ app/src/main/java/ru/samitin/allmovies/model/repository/RepositoryImpl.kt
@@ -1,10 +1,12 @@
 package ru.samitin.allmovies.model.repository
 
+import android.annotation.SuppressLint
 import android.content.res.Resources
 import android.content.res.TypedArray
 import ru.samitin.allmovies.R
 import ru.samitin.allmovies.model.data.Movie
 
+@SuppressLint("ResourceType")
 class RepositoryImpl(val resources:Resources= Resources.getSystem()) :Repository{
 
     private val movies:List<Movie>
@@ -12,10 +14,15 @@ class RepositoryImpl(val resources:Resources= Resources.getSystem()) :Repository
         val names=resources.getStringArray(R.array.titles);
         val ratings=resources.getIntArray(R.array.ratings)
         val dates=resources.getStringArray(R.array.dates)
-        movies= ArrayList<Movie>()
         val images:TypedArray =resources.obtainTypedArray(R.array.images)
-        for (i in 0..(names.size-1))
-            movies+Movie(name = names[i],images.getResourceId(i,0),reting = ratings[i],date=dates[i])
+        movies= listOf<Movie>(Movie(name = names[0],images.getResourceId(0,0),reting = ratings[0],date=dates[0]),
+                Movie(name = names[1],images.getResourceId(1,0),reting = ratings[1],date=dates[1]),
+                Movie(name = names[2],images.getResourceId(2,0),reting = ratings[2],date=dates[2]),
+                Movie(name = names[3],images.getResourceId(3,0),reting = ratings[3],date=dates[3]),
+                Movie(name = names[4],images.getResourceId(4,0),reting = ratings[4],date=dates[4]))
+
+
+
     }
     override fun getMoviesFromLocalStorage(): List<Movie> = movies
     override fun getMoviesFromServer(): List<Movie> = movies
--- app/src/main/java/ru/samitin/allmovies/view/HomeFragment.kt
+++ app/src/main/java/ru/samitin/allmovies/view/HomeFragment.kt
@@ -6,7 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import ru.samitin.allmovies.R
+import ru.samitin.allmovies.model.repository.Repository
+import ru.samitin.allmovies.model.repository.RepositoryImpl
 import ru.samitin.allmovies.viewmodel.MainViewModel
 
 class HomeFragment : Fragment() {
@@ -19,9 +23,17 @@ class HomeFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view=inflater.inflate(R.layout.fragment_home, container, false)
+        val reciclerView=view.findViewById<RecyclerView>(R.id.recycler_view_lines)
+        initReciclerView(reciclerView,RepositoryImpl(resources))
+        return view
     }
-
+    private fun initReciclerView(recyclerView: RecyclerView,repository: Repository){
+        val text=repository.getMoviesFromLocalStorage()[0].name
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager=LinearLayoutManager(context,LinearLayoutManager.HORIZONTAL,false)
+        recyclerView.adapter=MoviesAdapter(repository)
+    }
 
 
 }
