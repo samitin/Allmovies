f8f03e10dab73b84e3f9a1e0eba41efd12e5f7a4
samitin <samitin.e@gmail.com>
2021-07-30 16:58:59 +1100

    решил сделать по новому. Создание репозитория

---
+++ app/src/main/java/ru/samitin/allmovies/model/data/Category.kt
@@ -0,0 +1,7 @@
+package ru.samitin.allmovies.model.data
+
+import android.content.res.Resources
+import android.content.res.TypedArray
+import ru.samitin.allmovies.R
+
+data class Category (val categoryName:String,val movies:List<Movie>)
--- app/src/main/java/ru/samitin/allmovies/model/repository/Repository.kt
+++ app/src/main/java/ru/samitin/allmovies/model/repository/Repository.kt
@@ -2,9 +2,9 @@ package ru.samitin.allmovies.model.repository
 
 import android.content.Context
 import android.content.res.Resources
+import ru.samitin.allmovies.model.data.Category
 import ru.samitin.allmovies.model.data.Movie;
 interface Repository {
-    fun getMoviesFromServer(): List<Movie>
-    fun getMoviesFromLocalStorage(): List<Movie>
-    fun getCategoryName():String
+    fun getMoviesFromServer(): List<Category>
+    fun getMoviesFromLocalStorage(): List<Category>
 }
