fe59171e280646ff3a4608b7f493bda542ae0bf2
samitin <samitin.e@gmail.com>
2021-07-30 14:44:29 +1100

    изменение цвета кнопки при нажатии

--- app/src/main/java/ru/samitin/allmovies/view/MainActivity.kt
+++ app/src/main/java/ru/samitin/allmovies/view/MainActivity.kt
@@ -1,5 +1,6 @@
 package ru.samitin.allmovies.view
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -7,8 +8,11 @@ import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.SearchView
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.google.android.material.button.MaterialButton
 import ru.samitin.allmovies.R
+import ru.samitin.allmovies.R.color.buttons_bugrount_color
 import ru.samitin.allmovies.databinding.MainActivityBinding
 
 
@@ -22,6 +26,7 @@ class MainActivity : AppCompatActivity() {
 
         if (savedInstanceState == null) {
             showFragment(HomeFragment())
+            setColorClickButton(binder.buttonHome)
         }
         initView()
     }
@@ -30,10 +35,23 @@ class MainActivity : AppCompatActivity() {
         clickNavigateButtons()
     }
     private fun clickNavigateButtons(){
-        binder.buttonHome.setOnClickListener { showFragment(HomeFragment()) }
-        binder.buttonFavorite.setOnClickListener { showFragment(FavoritesFragment()) }
-        binder.buttonRatings.setOnClickListener { showFragment(RatingsFragment()) }
+        binder.buttonHome.setOnClickListener {
+            setColorClickButton(binder.buttonHome)
+            showFragment(HomeFragment()) }
+        binder.buttonFavorite.setOnClickListener {
+            setColorClickButton( binder.buttonFavorite)
+            showFragment(FavoritesFragment()) }
+        binder.buttonRatings.setOnClickListener {
+            setColorClickButton(binder.buttonRatings)
+            showFragment(RatingsFragment()) }
     }
+
+        private fun setColorClickButton(button: MaterialButton){
+            binder.buttonHome.backgroundTintList=ContextCompat.getColorStateList(this, R.color.white)
+            binder.buttonFavorite.backgroundTintList=ContextCompat.getColorStateList(this,R.color.white)
+            binder.buttonRatings.backgroundTintList=ContextCompat.getColorStateList(this,R.color.white)
+            button.backgroundTintList=ContextCompat.getColorStateList(this,R.color.buttons_bugrount_color)
+        }
 
     private fun showFragment(fragment:Fragment){
         supportFragmentManager.beginTransaction()
