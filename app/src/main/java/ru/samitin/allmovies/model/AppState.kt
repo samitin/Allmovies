60c7d07e049bb68c2e2c22a8e2d073f1f9663b6a
samitin <samitin.e@gmail.com>
2021-07-30 18:28:46 +1100

    делаем все при помощи ModelView

--- app/src/main/java/ru/samitin/allmovies/model/AppState.kt
+++ app/src/main/java/ru/samitin/allmovies/model/AppState.kt
@@ -4,7 +4,7 @@ import ru.samitin.allmovies.model.data.Category
 import ru.samitin.allmovies.model.data.Movie
 
 sealed class AppState{
-    data class Success(val movieData: List<Category>): AppState()
+    data class Success(val listCategory:List<Category>): AppState()
     data class Error(val error: Unit): AppState()
     object Loading: AppState()
 }
--- app/src/main/java/ru/samitin/allmovies/view/HomeFragment.kt
+++ app/src/main/java/ru/samitin/allmovies/view/HomeFragment.kt
@@ -10,13 +10,17 @@ import android.widget.LinearLayout
 import android.widget.ListView
 import android.widget.ScrollView
 import android.widget.TextView
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import ru.samitin.allmovies.R
 import ru.samitin.allmovies.databinding.FragmentHomeBinding
+import ru.samitin.allmovies.model.AppState
 import ru.samitin.allmovies.model.data.Category
 import ru.samitin.allmovies.model.repository.Repository
 import ru.samitin.allmovies.model.repository.RepositoryImpl
+import ru.samitin.allmovies.viewmodel.MainViewModel
 
 
 class HomeFragment : Fragment() {
@@ -27,21 +31,51 @@ class HomeFragment : Fragment() {
     private var _bainding:FragmentHomeBinding?=null
     private val binding
         get()=_bainding!!
+    private lateinit var viewModel: MainViewModel
     private lateinit var repository:Repository
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
         _bainding= FragmentHomeBinding.inflate(inflater,container,false)
         repository=RepositoryImpl()
-        initRepository(repository)
+
 
         return binding.root
     }
-    private fun initRepository(repository: Repository){
-        for (categegory in repository.getMoviesFromLocalStorage())
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val observer= Observer<AppState> {renderData(it)  }
+        viewModel.getLifeData().observe(viewLifecycleOwner,observer)
+        viewModel.getWeatherFromLocalSource()
+    }
+    private fun renderData(appState: AppState) {
+        when(appState){
+            is AppState.Success ->{
+                val listCategory=appState.listCategory
+                binding.loadingLayout.visibility=View.GONE
+                initListCategory(listCategory)
+            }
+            is AppState.Loading ->{
+                binding.loadingLayout.visibility=View.VISIBLE
+            }
+            is AppState.Error  ->{
+                binding.loadingLayout.visibility=View.GONE
+                Snackbar
+                        .make(binding.parentLayout, "Error", Snackbar.LENGTH_INDEFINITE)
+                        .setAction("Reload") { viewModel.getWeatherFromLocalSource() }
+                        .show()
+
+            }
+        }
+    }
+    private fun initListCategory(listCategery:List<Category>){
+        for (categegory in listCategery)
             binding.parentLayout.addView(createCategory(categegory))
     }
     private fun createCategory(category: Category):LinearLayout{
