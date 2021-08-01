bdfff5f6c4dd5da64a09370515e4c823c27811c1
samitin <samitin.e@gmail.com>
2021-08-01 17:19:14 +1100

    добавление в Movie.kt описание и трейлер

--- app/src/main/java/ru/samitin/allmovies/model/data/Movie.kt
+++ app/src/main/java/ru/samitin/allmovies/model/data/Movie.kt
@@ -1,3 +1,3 @@
 package ru.samitin.allmovies.model.data
 
-data class Movie(val name:String,val image:Int,val reting:Int,val date:String)
+data class Movie(val name:String,val image:Int,val reting:Int,val date:String,val descreotion:String,val trailer:String)
